c8b6f267ff87e0af7e73536eca3662239ed5dca2
mariocr94 <marioacr94@gmail.com>
2018-03-07 13:49:56 -0600

    Carga inicial de archivos

---
+++ Taller/Assets/cube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void mirarCubo(){
+		Debug.Log ("Diste click en el cubo");
+	}
+		
+}
